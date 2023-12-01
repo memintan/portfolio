55ef9de0d81b1f2a4c21427f5a00ce4065a01259
memintan <memintan@gmail.com>
2023-12-01 17:43:18 -0600

    projects module has almost completed

--- projects-page/scripts.js
+++ projects-page/scripts.js
@@ -1,42 +1,14 @@
-
-
-
-
-
-
-
 function toggleDetails(detailsId) {
-    var details = document.getElementById(detailsId);
-    var card = details.closest('.project-card');
+    var detailsContent = document.getElementById(detailsId).innerHTML;
+    var overlay = document.getElementById('overlay');
+    var content = overlay.querySelector('.overlay-content');
 
-    if (details.style.display === 'block') {
-        details.style.display = 'none';
-        card.style.flex = '0 1 30%'; // Reset the flex-basis to original
-    } else {
-        details.style.display = 'block';
-        card.style.flex = '0 1 80%'; // Expand the card
-    }
-
-    // Optional: Scroll the expanded card into view
-    card.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    // Inject details into overlay content
+    content.innerHTML = '<button class="btn-close" onclick="toggleOverlay()">Close</button>' + detailsContent;
+    overlay.style.display = 'flex';
 }
 
-
-//function toggleDetails(detailsId) {
-//    var details = document.getElementById(detailsId);
-//
-//    if (details.style.display === 'block') {
-//        details.style.display = 'none';
-//    } else {
-//        // Close any already open details
-//        var openDetails = document.querySelectorAll('.project-details');
-//        openDetails.forEach(function(detail) {
-//            detail.style.display = 'none';
-//        });
-//        // Show the clicked details
-//        details.style.display = 'block';
-//    }
-//
-//    // Optional: Scroll the expanded card into view
-//    document.querySelector('.projects-container').scrollIntoView({ behavior: 'smooth', block: 'start' });
-//}
+function toggleOverlay() {
+    var overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+}
