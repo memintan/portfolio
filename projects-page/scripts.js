56ee5fec08ae4e6a04028d155f6fb21e4840d897
memintan <memintan@gmail.com>
2023-12-01 16:58:10 -0600

    projects module has almost completed

--- projects-page/scripts.js
+++ projects-page/scripts.js
@@ -1,4 +1,10 @@
 
+
+
+
+
+
+
 function toggleDetails(detailsId) {
     var details = document.getElementById(detailsId);
     var card = details.closest('.project-card');
@@ -8,10 +14,29 @@ function toggleDetails(detailsId) {
         card.style.flex = '0 1 30%'; // Reset the flex-basis to original
     } else {
         details.style.display = 'block';
-        card.style.flex = '0 1 100%'; // Expand the card
+        card.style.flex = '0 1 80%'; // Expand the card
     }
 
     // Optional: Scroll the expanded card into view
     card.scrollIntoView({ behavior: 'smooth', block: 'start' });
 }
 
+
+//function toggleDetails(detailsId) {
+//    var details = document.getElementById(detailsId);
+//
+//    if (details.style.display === 'block') {
+//        details.style.display = 'none';
+//    } else {
+//        // Close any already open details
+//        var openDetails = document.querySelectorAll('.project-details');
+//        openDetails.forEach(function(detail) {
+//            detail.style.display = 'none';
+//        });
+//        // Show the clicked details
+//        details.style.display = 'block';
+//    }
+//
+//    // Optional: Scroll the expanded card into view
+//    document.querySelector('.projects-container').scrollIntoView({ behavior: 'smooth', block: 'start' });
+//}
