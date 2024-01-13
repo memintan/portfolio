61f64945f63cc82959d766cc700b6baf5dce8351
memintan <memintan@gmail.com>
2024-01-13 03:24:59 -0600

    WebConfig.java and WeatherController.java are updated

--- weatherService/src/main/java/org/mehmet/config/WebConfig.java
+++ weatherService/src/main/java/org/mehmet/config/WebConfig.java
@@ -6,14 +6,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/weather/**").allowedOrigins("https://memintan.github.io"); // Adjust the port if needed
+                // Adjust the allowed methods and headers as necessary
+                registry.addMapping("/weather/**")
+                        .allowedOrigins("https://memintan.github.io")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false); // Set to true only if you need cookies or authorization headers
             }
         };
     }
